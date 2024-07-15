014583b3e96f15100cc882696e0595e2f116bf6a
AndikaPutra315 <ajidwiandika567@gmai.com>
2024-07-15 11:53:34 +0800

    Membuat Struktur Data dan Fungsi Dasar

---
+++ taman.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+struct Pengunjung {
+    string nama;
+    int usia;
+    string jenisKelamin;
+    string nomorTelepon;
+    string tanggalKunjungan;
+    string jenisTiket;
+    string kodeBooking;
+};
+
+vector<Pengunjung> pengunjungList;
+
+string generateKodeBooking() {
+    string kode = "TW";
+    for (int i = 0; i < 5; ++i) {
+        kode += '0' + rand() % 10;
+    }
+    return kode;
+}
